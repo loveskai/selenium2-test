542d0588347b6d7be1bf1751d9f1d12f86045804
Shih-Kai Lin <shihkai.lin@openx.com>
2017-12-02 23:42:53 -0800

    use ui to search flight tickets

--- src/main/java/com/loveskai/Selenium2Example.java
+++ src/main/java/com/loveskai/Selenium2Example.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -11,10 +14,46 @@ import java.util.List;
  * This is a simple program to fetch airline name and its price by a given Expedia query link.
  */
 public class Selenium2Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
-        String url = "https://www.expedia.com/Flights-Search?flight-type=on&starDate=02%2F14%2F2018&endDate=02%2F19%2F2018&_xpid=11905%7C1&mode=search&trip=roundtrip&leg1=from%3ALos+Angeles%2C+CA+%28LAX-Los+Angeles+Intl.%29%2Cto%3ATaipei%2C+Taiwan+%28TPE-All+Airports%29%2Cdeparture%3A02%2F14%2F2018TANYT&leg2=from%3ATaipei%2C+Taiwan+%28TPE-All+Airports%29%2Cto%3ALos+Angeles%2C+CA+%28LAX-Los+Angeles+Intl.%29%2Cdeparture%3A02%2F19%2F2018TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY&options=cabinclass%3Apremium%2C&searchPriorityOverride=213";
-        driver.get(url);
+
+        String expediaHomePage = "https://www.expedia.com/";
+        String origin = "LAX";
+        String destination = "TPE";
+        String departing = "02/15/2018";
+        String returning = "02/19/2018";
+        String numAdults = "1";
+        String preferredClass = "premium";
+
+        driver.get(expediaHomePage);
+
+        driver.findElement(By.id("tab-flight-tab-hp")).click();
+
+        Select adultsSelect = new Select(driver.findElement(By.id("flight-adults-hp-flight")));
+        adultsSelect.selectByValue(numAdults);
+
+        // fill the returning input before departing input to prevent auto filling returning input
+        WebElement returningInput = driver.findElement(By.id("flight-returning-hp-flight"));
+        returningInput.sendKeys(returning);
+
+        WebElement departingInput = driver.findElement(By.id("flight-departing-hp-flight"));
+        departingInput.sendKeys(departing);
+
+        WebElement originInput = driver.findElement(By.id("flight-origin-hp-flight"));
+        originInput.sendKeys(origin);
+
+        WebElement destinationInput = driver.findElement(By.id("flight-destination-hp-flight"));
+        destinationInput.sendKeys(destination);
+
+        driver.findElement(By.id("flight-advanced-options-hp-flight")).click();
+
+        Select preferredClassSelect = new Select(driver.findElement(By.id("flight-advanced-preferred-class-hp-flight")));
+        preferredClassSelect.selectByValue(preferredClass);
+
+        driver.findElement(By.id("gcw-flights-form-hp-flight")).submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        wait.until(ExpectedConditions.attributeContains(By.id("pi-interstitial"), "style", "display: none;"));
 
         WebElement flightListingContainer = driver.findElement(By.id("flight-listing-container"));
         List<WebElement> flightElementList = flightListingContainer
@@ -27,8 +66,7 @@ public class Selenium2Example {
             WebElement priceElement = flightElement.findElement(By.cssSelector("div[class*='price-column']"));
             String price = priceElement.getAttribute("data-test-price-per-traveler");
             if (price != null) {
-                System.out.print(++numFlightWithPrice + ". ");
-                System.out.println(airlineName + ": " + price);
+                System.out.println(++numFlightWithPrice + ". " + airlineName + ": " + price);
             }
         }
 
